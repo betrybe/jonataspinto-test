94df58afb3b85223dd6024281b7c097296fa4ff4
jonataspinto <jonatastdb_jp@hotmail.com>
2021-11-10 18:44:32 -0300

    feat: criando reducer de wallet.

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,35 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import TYPES from '../constants';
+
+const initialState = {
+  currencies: [],
+  expenses: [],
+  status: TYPES.STATUS_IDLE,
+  error: null,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+  case TYPES.LOAD_WALLET:
+    return {
+      ...state,
+      status: TYPES.STATUS_LOADING,
+    };
+  case TYPES.LOAD_WALLET_SUCCESS:
+    return {
+      ...state,
+      ...action.payload,
+      status: TYPES.STATUS_SUCCESS,
+    };
+  case TYPES.LOAD_WALLET_ERROR:
+    return {
+      error: action.payload,
+      status: TYPES.STATUS_ERROR,
+    };
+
+  default:
+    return state;
+  }
+};
+
+export default reducer;
