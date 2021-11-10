3476b84bf9d68fe8fab1f59af51206d479f52a4e
jonataspinto <jonatastdb_jp@hotmail.com>
2021-11-10 17:31:40 -0300

    feat: criando reducer de usuario.

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,34 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+import TYPES from '../constants';
+
+const initialState = {
+  email: null,
+  status: TYPES.STATUS_IDLE,
+  error: null,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+  case TYPES.LOGIN_REQUEST:
+    return {
+      ...state,
+      status: TYPES.STATUS_LOADING,
+    };
+  case TYPES.LOGIN_SUCCESS:
+    return {
+      ...state,
+      user: action.payload,
+      status: TYPES.STATUS_SUCCESS,
+    };
+  case TYPES.LOGIN_ERROR:
+    return {
+      error: action.payload,
+      status: TYPES.STATUS_ERROR,
+    };
+
+  default:
+    return state;
+  }
+};
+
+export default reducer;
